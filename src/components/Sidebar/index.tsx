96b7e404a77bac35337e4c2b079c6e07f62bc454
WesleyMaik <wesleynamikaze404@gmail.com>
2024-01-07 17:15:17 -0400

    fix: change react-feather to radix-icons

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,26 +1,26 @@
-import { Settings as SettingsIcon, User as UserIcon } from "react-feather";
+import { GearIcon, PersonIcon } from "@radix-ui/react-icons";
 
 function Sidebar() {
-  const borderedStyle =
-    "w-full flex items-center gap-2 border border-zinc-300 dark:border-zinc-500 rounded-md p-4 hover:bg-zinc-200 dark:hover:bg-zinc-700 cursor-pointer transition ease-linear select-none";
+	const borderedStyle =
+		"w-full flex items-center gap-2 border border-zinc-300 dark:border-zinc-500 rounded-md p-4 hover:bg-zinc-200 dark:hover:bg-zinc-700 cursor-pointer transition ease-linear select-none";
 
-  return (
-    <div className="h-full flex flex-col gap-2 bg-zinc-100 dark:bg-zinc-800 p-4">
-      <div id="profile">
-        <button type="button" className={borderedStyle}>
-          <UserIcon strokeWidth={1.5} />
-          <span>Guest</span>
-        </button>
-      </div>
-      <div id="notes" className={borderedStyle}></div>
-      <div id="settings" className="mt-auto">
-        <button type="button" className={borderedStyle}>
-          <SettingsIcon strokeWidth={1.5} />
-          <span>Settings</span>
-        </button>
-      </div>
-    </div>
-  );
+	return (
+		<div className="h-full flex flex-col gap-2 bg-zinc-100 dark:bg-zinc-800 p-4">
+			<div id="profile">
+				<button type="button" className={borderedStyle}>
+					<GearIcon />
+					<span>Guest</span>
+				</button>
+			</div>
+			<div id="notes" className={borderedStyle}></div>
+			<div id="settings" className="mt-auto">
+				<button type="button" className={borderedStyle}>
+					<PersonIcon />
+					<span>Settings</span>
+				</button>
+			</div>
+		</div>
+	);
 }
 
 export default Sidebar;
