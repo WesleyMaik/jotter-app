422980b1d212b4b0885818c556b81a94700ce005
WesleyMaik <wesleynamikaze404@gmail.com>
2024-01-07 17:14:48 -0400

    feat: add classNames props to customize

--- src/components/Menu/Dropdown.tsx
+++ src/components/Menu/Dropdown.tsx
@@ -1,71 +1,96 @@
 "use client";
 
 import { useEffect, useRef, useState } from "react";
-import { Menu as MenuIcon } from "react-feather";
+import { HamburgerMenuIcon as MenuIcon } from "@radix-ui/react-icons";
 import { Item } from "./props.types";
 import MenuItem from "./MenuItem";
 
 import setElementInScreen from "@/utils/setElementInScreen";
 import useClickOutside from "@/hooks/useClickOutside";
+import clsx from "clsx";
 
 interface DropdownProps {
-  /*
-   *  Text or Element to trigger dropdown menu.
-   */
-  label?: string;
-  /*
-   * Items to appear inside of dropdown menu.
-   */
-  items: Item[];
+	/*
+	 *  Text or Element to trigger dropdown menu.
+	 */
+	label?: string;
+	/*
+	 * Items to appear inside of dropdown menu.
+	 */
+	items: Item[];
+	/*
+	 * Trigger's class name.
+	 */
+	triggerClassName?: string;
+	/*
+	 * Menu Container's class name.
+	 */
+	menuClassName?: string;
+	/*
+	 * Items's class name.
+	 */
+	itemClassName?: string;
 }
 
-function Dropdown({ items, label }: DropdownProps) {
-  const [isOpen, setIsOpen] = useState(false);
-  const menuRef = useRef<HTMLDivElement>(null);
+function Dropdown({
+	items,
+	label,
+	triggerClassName,
+	menuClassName,
+	itemClassName,
+}: DropdownProps) {
+	const [isOpen, setIsOpen] = useState(false);
+	const menuRef = useRef<HTMLDivElement>(null);
 
-  const toggleMenu = () => setIsOpen((state) => !state);
-  const closeMenu = () => setIsOpen(false);
+	const toggleMenu = () => setIsOpen((state) => !state);
+	const closeMenu = () => setIsOpen(false);
 
-  useClickOutside({
-    ref: menuRef,
-    callback: closeMenu,
-  });
+	useClickOutside({
+		ref: menuRef,
+		callback: closeMenu,
+	});
 
-  useEffect(() => {
-    if (!isOpen) {
-      return;
-    }
+	useEffect(() => {
+		if (!isOpen) {
+			return;
+		}
 
-    setElementInScreen(menuRef.current);
-  }, [isOpen]);
+		setElementInScreen(menuRef.current);
+	}, [isOpen]);
 
-  return (
-    <div
-      id="dropdown-menu"
-      className="flex justify-center items-center flex-col relative"
-    >
-      <button
-        type="button"
-        aria-label="Choose a option"
-        aria-haspopup="menu"
-        className="active:scale-[0.95] transition-[scale]"
-        onClick={toggleMenu}
-      >
-        {label ? label : <MenuIcon />}
-      </button>
-      {isOpen ? (
-        <div
-          id="menu"
-          ref={menuRef}
-          className="absolute top-[100%] bottom-auto bg-zinc-100 dark:bg-zinc-900 p-4 rounded-lg"
-        >
-          {items?.map((item, key) => (
-            <MenuItem {...item} key={key} />
-          ))}
-        </div>
-      ) : null}
-    </div>
-  );
+	return (
+		<div
+			id="dropdown-menu"
+			className="flex justify-center items-center flex-col relative"
+		>
+			<button
+				type="button"
+				aria-label="Choose a option"
+				aria-haspopup="menu"
+				className={clsx(
+					"active:scale-[0.95] transition-[scale]",
+					triggerClassName
+				)}
+				onClick={toggleMenu}
+			>
+				{label ? label : <MenuIcon />}
+			</button>
+			{isOpen ? (
+				<div
+					id="menu"
+					ref={menuRef}
+					className={clsx(
+						"absolute top-[100%] bottom-auto bg-zinc-100 dark:bg-zinc-900 p-4 rounded-lg shadow-lg",
+						menuClassName
+					)}
+				>
+					{items?.map((item, key) => (
+						<MenuItem {...item} key={key} itemClassName={itemClassName} />
+					))}
+				</div>
+			) : null}
+		</div>
+	);
 }
 
 export default Dropdown;
--- src/components/Menu/MenuItem.tsx
+++ src/components/Menu/MenuItem.tsx
@@ -1,26 +1,31 @@
+import clsx from "clsx";
 import { Item } from "./props.types";
 
-type MenuItemProps = Item;
+type MenuItemProps = Item & {
+	itemClassName?: string;
+};
 
-function MenuItem({ label, action }: MenuItemProps) {
-  const className =
-    "w-full text-nowrap hover:bg-zinc-200 dark:hover:bg-zinc-700 px-2 rounded-sm cursor-pointer select-none active:scale-[0.95] transition-[scale]";
+function MenuItem({ label, action, itemClassName }: MenuItemProps) {
+	const className = clsx(
+		"w-full text-nowrap hover:bg-zinc-200 dark:hover:bg-zinc-700 px-2 rounded-sm cursor-pointer select-none active:scale-[0.95] transition-[scale]",
+		itemClassName
+	);
 
-  const props = {
-    className,
-    role: "menuitem",
-    children: label,
-  };
+	const props = {
+		className,
+		role: "menuitem",
+		children: label,
+	};
 
-  if (!action) {
-    return <p {...props} />;
-  }
+	if (!action) {
+		return <p {...props} />;
+	}
 
-  if (typeof action == "object") {
-    return <a {...props} href={action.url} />;
-  }
+	if (typeof action == "object") {
+		return <a {...props} href={action.url} />;
+	}
 
-  return <button {...props} type="button" onClick={action} />;
+	return <button {...props} type="button" onClick={action} />;
 }
 
 export default MenuItem;
